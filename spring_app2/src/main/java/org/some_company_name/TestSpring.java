af48248da7d504a4343b35fde509616d22e45f55
Dmitrii Chernyshov <ragnarzone@gmail.com>
2022-05-20 14:26:17 +0200

    separated scratches from study things

---
+++ spring_app2/src/main/java/org/some_company_name/TestSpring.java
@@ -0,0 +1,19 @@
+package org.some_company_name;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class TestSpring {
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
+                "applicationContext.xml"
+        );
+
+        Music music = context.getBean("musicBean", Music.class);
+
+        MusicPlayer musicPlayer = new MusicPlayer(music);
+
+        musicPlayer.playMusic();
+
+        context.close();
+    }
+}
