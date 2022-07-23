90ccff5ac7d1068e16614c6bc35e7cd9901d1823
ragnarzone <ragnarzone@gmail.com>
2022-07-23 08:04:53 +0300

    added simple enum use example

---
+++ sources_23.07.2022/enumerated_types/SimpleEnumUse.java
@@ -0,0 +1,9 @@
+package enumerated_types;
+
+// to use enum we create a reference of that type and assign it to an instance
+public class SimpleEnumUse {
+    public static void main(String[] args) {
+        Spiciness howHot = Spiciness.MEDIUM;
+        System.out.println(howHot);
+    }
+}
---
+++ sources_23.07.2022/enumerated_types/Spiciness.java
@@ -0,0 +1,6 @@
+package enumerated_types;
+
+// instances of enumerated types are constants
+public enum Spiciness {
+    NOT, MILD, MEDIUM, HOT, FLAMING
+}
