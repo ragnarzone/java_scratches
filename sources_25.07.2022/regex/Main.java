32ca9e59262f58c2ad3068694d4731dc34988dcb
ragnarzone <ragnarzone@gmail.com>
2022-07-25 14:50:22 +0300

    added example to show simple regex matching

---
+++ sources_25.07.2022/regex/Main.java
@@ -0,0 +1,15 @@
+package regex;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Main {
+    public static void main(String[] args) {
+        Pattern p = Pattern.compile("a");
+        Matcher m = p.matcher("Jack is a boy");
+        while (m.find()){
+            System.out.print(m.start() + " " + m.group() + " ");
+        }
+        System.out.println();
+    }
+}
